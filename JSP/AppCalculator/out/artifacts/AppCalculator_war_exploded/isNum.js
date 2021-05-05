4968ae632231b8e4f2e79c1e06ce7f3f32d1c6e9
NHNJoker <ngonhanit@gmail.com>
2021-05-05 20:17:11 +0700

    fix loi bai app calculator

--- JSP/AppCalculator/out/artifacts/AppCalculator_war_exploded/isNum.js
+++ JSP/AppCalculator/out/artifacts/AppCalculator_war_exploded/isNum.js
@@ -1,19 +1,19 @@
 function isNum() {
-    let checkIsNum = new RegExp("/^([0-9]+.[0-9]+)|([0-9]+)$/")
-    let firstOperand = document.getElementById("firstOperand")
-    let secondOperand = document.getElementById("secondOperand")
-    if (!firstOperand ==="" && !secondOperand ===""){
+    let checkIsNum = new RegExp("([0-9]+.[0-9]+)|([0-9]+)")
+    let firstOperand = document.getElementById("firstOperand").value;
+    let secondOperand = document.getElementById("secondOperand").value;
+    if (!(firstOperand ==="") && !(secondOperand ==="")){
         if (!checkIsNum.test(firstOperand)) {
-            alert("Vui lòng nhập vào một số.");
+            alert("Vui lòng nhập vào một số.1");
             return false;
         }
-        if ( !checkIsNum.test(secondOperand)){
-            alert("Vui lòng nhập vào một số.");
+        if (!checkIsNum.test(secondOperand)){
+            alert("Vui lòng nhập vào một số.2");
             return false;
         }
         return true;
     }else {
-        alert("Vui lòng nhập vào một số.");
+        alert("Vui lòng nhập vào một số.3");
         return false;
     }
 }
